4b5445510d5fda0a1bd489c8b710fb19769faa78
tsukker <tsukker@users.noreply.github.com>
2020-07-29 07:07:41 +0900

    Improve UX

--- json_from_path.py
+++ json_from_path.py
@@ -10,27 +10,45 @@ import requests
 
 from api import Api
 
+
+def select_path():
+    options = [
+        'results',
+        'coop_results',
+    ]
+    for i, path in enumerate(options):
+        print('{} : {}'.format(i + 1, path))
+    print('Please select (default: 1) : ', end='')
+    s = input()
+    ret = options[0]
+    if s == '2':
+        ret = options[1]
+    return ret
+
+
 if __name__ == '__main__':
     api = Api()
     print('path:')
-    path = input()
+    path = select_path()
     if path == '':
         path = '/results'
     elif path[0] != '/':
         path = '/' + path
+    print('path: "{}"'.format(path))
     resp = api.request(path)
     assert (resp.status_code == http.HTTPStatus.OK)
     if getattr(sys, 'frozen', False):
         app_path = os.path.dirname(sys.executable)
     elif __file__:
         app_path = os.path.dirname(__file__)
-    print('json filename:')
-    filename = input()
+    json_dir = 'json'
+    json_path = os.path.join(app_path, json_dir)
+    filename = ''
     if filename == '':
         filename = '{}_{}.json'.format(
             path.replace('/', ''),
             dt.datetime.now().strftime('%Y%m%d%H%M'))
-    filepath = os.path.join(app_path, filename)
+    filepath = os.path.join(json_path, filename)
     f = open(filepath, 'w')
     json.dump(
         resp.json(),
